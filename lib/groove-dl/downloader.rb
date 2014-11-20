cd38c426b7bc3fc8b5eb2907084cc1be364c8190
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-11-20 14:36:18 +0100

    Add dockblock

--- lib/groove-dl/downloader.rb
+++ lib/groove-dl/downloader.rb
@@ -7,6 +7,11 @@ module GrooveDl
     attr_writer :download_count
     attr_writer :download_skip
 
+    ##
+    # Initialize download
+    #
+    # @return [Downloader]
+    #
     def initialize(client, options = {})
       @client = client
       @output_directory = options[:o] || Dir.tmpdir
@@ -15,6 +20,13 @@ module GrooveDl
       @skip = 0
     end
 
+    ##
+    # Download playlist
+    #
+    # @params [String] playlist_id Playlist id
+    #
+    # @return [Array]
+    #
     def playlist(playlist_id)
       playlist = @client.request('getPlaylistByID',
                                  playlistID: playlist_id)
@@ -26,6 +38,14 @@ module GrooveDl
       download_queue
     end
 
+    ##
+    # Download song
+    #
+    # @param [Grooveshark::Song] song Song object
+    # @param [String] filename Where the file should be downloaded
+    #
+    # @return [Net::HTTP]
+    #
     def download(song, filename)
       url = URI.parse(@client.get_song_url_by_id(song.id))
       @client.get_stream_auth_by_songid(song.id)
@@ -33,9 +53,14 @@ module GrooveDl
       RestClient::Request
         .execute(method: :get,
                  url: url.to_s,
-                 block_response: process_response(filename))
+                 block_response: process_response(filename)).class
     end
 
+    ##
+    # Download queue
+    #
+    # @return [false|Array]
+    #
     def download_queue
       return false if @queue.empty?
       @queue.each do |song|
@@ -53,12 +78,20 @@ module GrooveDl
       { skipped: @skip, downloaded: @count }
     end
 
-    def process_response(filename)
+    ##
+    # Process response to display a progress bar and download
+    # file into destination.
+    #
+    # @param [String] destination Destination
+    #
+    # @return [Proc]
+    #
+    def process_response(destination)
       proc do |response|
-        pbar = ProgressBar.create(title: filename.split('/').last,
+        pbar = ProgressBar.create(title: destination.split('/').last,
                                   format: '%a |%b>>%i| %p%% %t',
                                   total: response['content-length'].to_i)
-        File.open(filename, 'w') do |f|
+        File.open(destination, 'w') do |f|
           response.read_body do |chunk|
             f.write(chunk)
             pbar.progress += chunk.length
